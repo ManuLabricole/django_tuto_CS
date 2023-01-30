b6817e3b185130472b17e22953bf3e7a57e66bce
Manu Labricole <manu_barriol@hotmail.fr>
2023-01-30 18:01:06 +0100

    update base.html with message alert

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -11,6 +11,7 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valide():
             username = form.cleaned_data.get("username")
-            messages.success(request, f"Account created for {username}")
+            messages.success(request, f"Account created for {username} !")
+            return redirect("blog-home")
     else:
         return render(request, "users/register.html", {"form": form})
