ebd66ad56210a414725f9e649ffca7d3e91b942e
Manu Labricole <manu_barriol@hotmail.fr>
2023-01-31 17:25:28 +0100

    handle default.jpg

--- users/models.py
+++ users/models.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default="default.png", upload_to="profile_pics")
+    image = models.ImageField(
+        default="doesn't take into account", upload_to="profile_pics")
 
     def __str__(self):
         return f'{self.user.username} Profile'
